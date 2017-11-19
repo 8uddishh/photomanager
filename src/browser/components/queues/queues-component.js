ea8604e2c7b8f0c16d3329b81873d80b17876087
SenthilKumar Baliah <senthil.baliah@liquidityservices.com>
2017-11-19 01:59:05 -0600

    queues screen done

--- src/browser/components/queues/queues-component.js
+++ src/browser/components/queues/queues-component.js
@@ -1,4 +1,5 @@
 import { baseComponent } from "./../base-component"
+import { accordionComponent } from "./../ux/accordion-component"
 
 export class queuesComponent extends baseComponent {
 
@@ -12,75 +13,136 @@ export class queuesComponent extends baseComponent {
 
     get $template(){
         return `
-    <div class="container header-container">
-        <div class="is-uppercase">
-            upload options
-        </div>
-    </div>
-    <div class="container default-container">
-        <ul class="vertical-flow">
-            <li>
-                <input class="with-gap" id="upld-by-file-name" name="upload-option" type="radio" checked>
-                <label for="upld-by-file-name">Identify Items By File Name</label>
-            </li>
-            <li>
-                <input class="with-gap" disabled id="upld-by-bar-code" name="upload-option" type="radio">
-                <label for="upld-by-bar-code">Identify Items By Bar code</label>
-            </li>
-            <li>
-                <input class="with-gap" id="upld-by-exif" name="upload-option" type="radio">
-                <label for="upld-by-exif">Identify Items By Exif Meta Data</label>
-            </li>
-            <li>
-                <input class="with-gap" id="upld-aft-crop" name="upload-option" type="radio">
-                <label for="upld-aft-crop">Enable Crop/Resize</label>
-            </li>
-          </ul>
-    </div>
-    <div class="container header-container">
-        <div class="is-uppercase">
-            upload to
-        </div>
-    </div>
-    <div class="container default-container">
-        <ul class="vertical-flow">
-          <li>
-              <input class="with-gap" id="idn-by-sku" name="identifier-option" type="radio" checked>
-              <label for="idn-by-sku">Identifier is SKU</label>
-          </li>
-          <li>
-              <input class="with-gap" disabled id="idn-by-dtid" name="identifier-option" type="radio">
-              <label for="idn-by-dtid">Identifier is DTID</label>
-          </li>
-          <li>
-              <input class="with-gap" id="idn-by-lot" name="identifier-option" type="radio">
-              <label for="idn-by-lot">Identifier is LOT</label>
-          </li>
-        </ul>
-    </div>
-    <div class="container status-container">
-        <div class="field is-grouped is-grouped-multiline">
-            <div class="control">
-              <div class="tags has-addons">
-                <span class="tag is-info">Total</span>
-                <span class="tag is-dark">0</span>
+        <div id="settings-accordion" class="accordion">
+        <div class="dropdown is-active">
+          <div class="dropdown-trigger">
+            <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
+              <div class="is-uppercase">folder(s) in queue</div>
+              <div class="icon">
+                <i class="fa fa-angle-down" aria-hidden="true"></i>
               </div>
-            </div>
-          
-            <div class="control">
-              <div class="tags has-addons">
-                <span class="tag is-success">Success</span>
-                <span class="tag is-dark">0</span>
-              </div>
-            </div>
-          
-            <div class="control">
-              <div class="tags has-addons">
-                <span class="tag is-danger">Failed</span>
-                <span class="tag is-dark">0</span>
-              </div>
-            </div>
+            </button>
           </div>
-    </div>`
+          <div class="dropdown-menu is-shadow" id="dropdown-menu" role="menu">
+          <ul class="dropdown-content vertical-flow in-progress-queues">
+            <li class="dropdown-item">
+              <div class="pull-left">
+                <div class="title padding-before-progress">
+                  Folder # 1
+                  <span class="tag is-danger">3/100</span>
+                  <span class="tag is-success">7/100</span>
+                </div>
+                <div class="subtitle">
+                    <progress class="progress is-success" value="15" max="100">30%</progress>
+                </div>
+              </div>
+            </li>
+            <li class="dropdown-item">
+              <div class="pull-left">
+              <div class="title padding-before-progress">
+                Folder # 2
+              </div>
+              <div class="subtitle" style="display:none">
+                <progress class="progress is-success" value="15" max="100">30%</progress>
+              </div>
+              </div>
+              <a class="button icon remove pull-right" ><i class="fa fa-minus-circle"></i></a>
+            </li>
+            <li class="dropdown-item">
+            <div class="pull-left">
+            <div class="title padding-before-progress">
+              Folder # 3
+            </div>
+            <div class="subtitle" style="display:none">
+              <progress class="progress is-success" value="15" max="100">30%</progress>
+            </div>
+            </div>
+            <a class="button icon remove pull-right" ><i class="fa fa-minus-circle"></i></a>
+          </li>
+          </ul>
+          </div>
+        </div>
+
+        <div class="dropdown accordion">
+        <div class="dropdown-trigger">
+          <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
+            <div class="is-uppercase">completed folder(s)</div>
+            <div class="icon">
+              <i class="fa fa-angle-down" aria-hidden="true"></i>
+            </div>
+          </button>
+        </div>
+        <div class="dropdown-menu is-shadow" id="dropdown-menu" role="menu">
+          <ul class="dropdown-content vertical-flow processed-queue">
+            <li class="dropdown-item">
+                <div class="pull-left">
+                    <div class="title">
+                    Folder # 1
+                    <span class="tag is-danger">30/100</span>
+                    <span class="tag is-success">70/100</span>
+                    </div>
+                    <div class="subtitle is-uppercase">
+                        today
+                    </div>
+                </div>
+            </li>
+            <li class="dropdown-item">
+            <div class="pull-left">
+                <div class="title">
+                Folder # 2
+                <span class="tag is-danger">30/100</span>
+                <span class="tag is-success">70/100</span>
+                </div>
+                <div class="subtitle is-uppercase">
+                    06/15/2017 09:15 am
+                </div>
+            </div>
+        </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="dropdown accordion">
+      <div class="dropdown-trigger">
+        <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
+          <div class="is-uppercase">saved folder(s)</div>
+          <div class="icon">
+            <i class="fa fa-angle-down" aria-hidden="true"></i>
+          </div>
+        </button>
+      </div>
+      <div class="dropdown-menu is-shadow" id="dropdown-menu" role="menu">
+      <ul class="dropdown-content vertical-flow">
+        <li class="dropdown-item">
+            <div class="pull-left">
+            <div class="title">
+                Folder # 1
+            </div>
+            <div class="subtitle is-uppercase">
+                read write delete
+            </div>
+            </div>
+            <a class="button icon run pull-right" ><i class="fa fa-chevron-circle-right"></i></a>
+        </li>
+        <li class="dropdown-item">
+            <div class="pull-left">
+            <div class="title">
+            Folder # 2
+            </div>
+            <div class="subtitle is-uppercase">
+            read write delete
+            </div>
+            </div>
+            <a class="button icon run pull-right" ><i class="fa fa-chevron-circle-right"></i></a>
+        </li>
+      </ul>
+      </div>
+    </div>
+      </div>`
     }
+
+    settifyWidgets () {
+        this.accordion = new accordionComponent(this.$el.querySelector("#settings-accordion"))
+        this.accordion.settify()
+      }
 }
