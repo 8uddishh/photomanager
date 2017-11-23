0fb32ac1346491329f1b5e1f2ae9111ebb9bcec7
8uddishh <senthil.baliah@gmail.com>
2017-11-23 01:37:50 -0600

    slide 4 layout

--- src/browser/components/setup/activate-component.js
+++ src/browser/components/setup/activate-component.js
@@ -34,10 +34,26 @@ export class activationComponent extends baseComponent {
                             <div class="pull-left">
                                 <span class="bar-code">710b962e-041c-11e1-9234-0123456789ab</span>
                             </div>
-                            <a class="button pull-right" ><i class="fa fa-copy"></i></a>
+                            <a class="button pull-right" id="select-auth-code" ><i class="fa fa-copy"></i></a>
                         </li>
                         <li>Paste the copied code into the BOT textbox and click generate</li>
                         <li>You would be displayed with an auth code</li>
+                    </ul>
+                </div>
+                <div id="act-step-3" class="interchange-item is-slide is-right">
+                    <h1 class="title">Step 3</h1>
+                    <ul class="vertical-flow setup">
+                        <li><p>Copy the displayed auth code in BOT by clicking <i class="fa fa-copy"></i> button</p></li>
+                        <li>
+                            <p>
+                                Paste the auth code in the textbox below and click 'Authenticate'
+                            </p>
+                            <div class="field">
+                                <div class="control">
+                                <input class="input is-info" type="password" placeholder="Info input">
+                                </div>
+                            </div>
+                        </li>
                     </ul>
                 </div>
             </div>
@@ -51,25 +67,33 @@ export class activationComponent extends baseComponent {
         document.querySelector("body").removeAttribute("class")
         document.querySelector("body").classList.add("activate")
         this.currentSlide = 0
-        this.slides = [ "#act-hello", "#act-step-1", "#act-step-2" ]
+        this.slides = [ "#act-hello", "#act-step-1", "#act-step-2", "#act-step-3" ]
+        this.applicationKeySelected = false
     }
 
     settifyTriggers () {
         document.querySelector("#prev-step").classList.add("hidden")
 
         document.querySelector("#next-step").addEventListener("click", e => {
-            this.$el.querySelector(this.slides[this.currentSlide]).classList.remove("is-active")
-            this.$el.querySelector(this.slides[this.currentSlide]).classList.add("is-slide", "is-left")
-
-            this.currentSlide++
-            
-            this.$el.querySelector(this.slides[this.currentSlide]).classList.remove("is-slide", "is-left", "is-right")
-            this.$el.querySelector(this.slides[this.currentSlide]).classList.add("is-active")
-
-            if(this.currentSlide == this.slides.length - 1)
-                document.querySelector("#next-step").classList.add("hidden")
-            
-            document.querySelector("#prev-step").classList.remove("hidden")
+            if(!e.target.hasAttribute("disabled")) {
+                this.$el.querySelector(this.slides[this.currentSlide]).classList.remove("is-active")
+                this.$el.querySelector(this.slides[this.currentSlide]).classList.add("is-slide", "is-left")
+    
+                this.currentSlide++
+                
+                this.$el.querySelector(this.slides[this.currentSlide]).classList.remove("is-slide", "is-left", "is-right")
+                this.$el.querySelector(this.slides[this.currentSlide]).classList.add("is-active")
+    
+                if(this.currentSlide == this.slides.length - 1)
+                    document.querySelector("#next-step").classList.add("hidden")
+                
+                document.querySelector("#prev-step").classList.remove("hidden")
+    
+                if(this.currentSlide == 2 && !this.applicationKeySelected)
+                    document.querySelector("#next-step").setAttribute("disabled", "")
+                else 
+                    document.querySelector("#next-step").removeAttribute("disabled")
+            }
         })
 
         document.querySelector("#prev-step").addEventListener("click", e => {
@@ -85,6 +109,12 @@ export class activationComponent extends baseComponent {
                 document.querySelector("#prev-step").classList.add("hidden")
 
             document.querySelector("#next-step").classList.remove("hidden")
+        })
+
+        this.$el.querySelector("#select-auth-code").addEventListener("click", e => {
+            this.applicationKeySelected = true
+            this.$el.querySelector("#select-auth-code").classList.add("is-active")
+            document.querySelector("#next-step").removeAttribute("disabled")
         })
     }
 }
